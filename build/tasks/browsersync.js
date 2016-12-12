bec373c5533cb9e8cba63e23d0b30d4721280e4a
Salem Ghoweri <salem.ghoweri@pega.com>
2016-12-12 11:37:12 -0500

    Updating browsersync to only auto-reload the contents of Pattern Lab's iframe, not the index itself.

--- build/tasks/browsersync.js
+++ build/tasks/browsersync.js
@@ -41,6 +41,9 @@ module.exports = function (gulp, config, $) {
           // }),
           // webpackHotMiddleware(bundler)
         ]
+      },
+      snippetOptions: {
+        blacklist: ['/index.html', '/', '/?*']
       }
     });
   });
